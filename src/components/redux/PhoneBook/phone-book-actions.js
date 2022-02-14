e98580aa3507b4b43701eef1c8f8581acd3e0cca
Oksana Stets <oksanastets73@gmail.com>
2022-02-14 21:37:21 +0200

    add files from previous work

--- src/components/redux/PhoneBook/phone-book-actions.js
+++ src/components/redux/PhoneBook/phone-book-actions.js
@@ -1,14 +1,16 @@
-import { nanoid } from 'nanoid';
+// import { nanoid } from 'nanoid';
 import { createAction } from '@reduxjs/toolkit';
 
-export const addContact = createAction('phonebook/addContact', (nick, number) => {
-  return {
-    payload: {
-      id: nanoid(),
-      nick: nick,
-      number: number,
-    },
-  };
-});
-export const deleteContact = createAction('phonebook/deleteContact');
+export const fetchContactsRequest = createAction('phonebook/fetchContactsRequest');
+export const fetchContactsSuccess = createAction('phonebook/fetchContactsSuccess');
+export const fetchContactsError = createAction('phonebook/fetchContactsError');
+
+export const addContactRequest = createAction('phonebook/addContactRequest');
+export const addContactSuccess = createAction('phonebook/addContactSuccess');
+export const addContactError = createAction('phonebook/addContactError');
+
+export const deleteContactRequest = createAction('phonebook/deleteContactRequest');
+export const deleteContactSuccess = createAction('phonebook/deleteContactSuccess');
+export const deleteContactError = createAction('phonebook/deleteContactError');
+
 export const changeFilter = createAction('phonebook/changeFilter');
--- src/components/redux/PhoneBook/phone-book-reducer.js
+++ src/components/redux/PhoneBook/phone-book-reducer.js
@@ -1,28 +1,56 @@
 import toast from 'react-hot-toast';
 import { combineReducers } from 'redux';
 import { createReducer } from '@reduxjs/toolkit';
-import {addContact, deleteContact, changeFilter } from './phone-book-actions';
-import initialContacts from '../../Phonebook/contacts.json';
+import {
+  addContactRequest,
+  addContactSuccess,
+  addContactError,
+  deleteContactRequest,
+  deleteContactSuccess,
+  deleteContactError,
+  // deleteContact,
+  changeFilter,
+  fetchContactsRequest,
+  fetchContactsSuccess,
+  fetchContactsError,
+} from './phone-book-actions';
 
-const contacts = createReducer(initialContacts, {
-    [addContact]: (state, { payload }) => { 
+// import initialContacts from '../../Phonebook/contacts.json';
+
+const contacts = createReducer([],
+  // initialContacts,
+  {
+    [fetchContactsSuccess]: (state, { payload }) => payload,
+    [addContactSuccess]: (state, { payload }) => { 
          if (state.find(contact => contact.nick === payload.nick)) {
         const notify = `${payload.nick} is already on list`;
         toast.error(notify);
         return state;
       }
       state = [...state, payload];
-      toast.success('Contact added!');
+      toast.success(`Contact ${payload.nick} added!`);
       return state;
     },
-    [deleteContact]: (state, {payload}) => state.filter(({ id }) => id !== payload),
+    [deleteContactSuccess]: (state, {payload}) => state.filter(({ id }) => id !== payload),
 })
 
 const filter = createReducer('', {
     [changeFilter]: (_, {payload})=> payload,
 })
-
+const loading = createReducer(false, {
+  [fetchContactsRequest]: () => true,
+  [fetchContactsSuccess]: () => false,
+  [fetchContactsError]: () => false,
+  [addContactRequest]: () => true,
+  [addContactSuccess]: () => false,
+  [addContactError]: () => false,
+  [deleteContactRequest]: () => true,
+  [deleteContactSuccess]: () => false,
+  [deleteContactError]: () => false,
+ 
+})
 export default combineReducers({
   contacts,
   filter,
+  loading,
 });
